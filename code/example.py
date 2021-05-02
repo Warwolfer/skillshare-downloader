a93f8566c9eb5b1ee1ace3f5c7316ae40e47c02e
Kenny Reynaldo <warwolfer@yahoo.com>
2021-05-02 22:13:13 +0700

    Fix invalid video id

--- code/example.py
+++ code/example.py
@@ -1,13 +1,15 @@
 from downloader import Downloader
 
 cookie = """
-ADD YOUR COOKIE HERE
+device_session_id=192a6e45-0b8e-44fe-a785-f0fa47386a0f; show-like-copy=0; visitor_tracking=utm_campaign%3D%26utm_source%3D%26utm_medium%3D%26utm_term%3D%26referrer%3Dhttps%3A%2F%2Fwww.google.com%2F%26referring_username%3D; first_landing=utm_campaign%3D%26utm_source%3D%26utm_medium%3D%26utm_term%3D%26referrer%3Dhttps%3A%2F%2Fwww.google.com%2F%26referring_username%3D; G_ENABLED_IDPS=google; g_state={\"i_l\":0}; ss_hide_default_banner=1619167156.712; dismiss_transcripts_tooltip=1; PHPSESSID=551ce23a7f020f21f5a75645330bc52e; YII_CSRF_TOKEN=VXJiTkFqUVAzRlhqWVJSWnJ5R2VwcVFVdmI3UkxiNnRfZiN0hXmzX1hT5ZEy9yqMP7MZIPN6Zwi1C3g_UL6aqQ%3D%3D; __stripe_mid=5e543a01-0549-4438-be44-0ab535657690a98519
 """
 
 dl = Downloader(cookie=cookie)
 
+text = input("Course link here : ")
+
 # download by class URL:
-dl.download_course_by_url('https://www.skillshare.com/classes/Art-Fundamentals-in-One-Hour/189505397')
+dl.download_course_by_url(text)
 
 # or by class ID:
 # dl.download_course_by_class_id(189505397)
