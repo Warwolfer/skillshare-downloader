468018080b93e114bb1e9044798c75d8a5edbb13
Kenny Reynaldo <warwolfer@yahoo.com>
2021-05-02 23:01:01 +0700

    Add english subtitle downloader support

--- code/example.py
+++ code/example.py
@@ -1,10 +1,13 @@
 from downloader import Downloader
 
+# ADD YOUR COOKIE HERE
 cookie = """
 device_session_id=192a6e45-0b8e-44fe-a785-f0fa47386a0f; show-like-copy=0; visitor_tracking=utm_campaign%3D%26utm_source%3D%26utm_medium%3D%26utm_term%3D%26referrer%3Dhttps%3A%2F%2Fwww.google.com%2F%26referring_username%3D; first_landing=utm_campaign%3D%26utm_source%3D%26utm_medium%3D%26utm_term%3D%26referrer%3Dhttps%3A%2F%2Fwww.google.com%2F%26referring_username%3D; G_ENABLED_IDPS=google; g_state={\"i_l\":0}; ss_hide_default_banner=1619167156.712; dismiss_transcripts_tooltip=1; PHPSESSID=551ce23a7f020f21f5a75645330bc52e; YII_CSRF_TOKEN=VXJiTkFqUVAzRlhqWVJSWnJ5R2VwcVFVdmI3UkxiNnRfZiN0hXmzX1hT5ZEy9yqMP7MZIPN6Zwi1C3g_UL6aqQ%3D%3D; __stripe_mid=5e543a01-0549-4438-be44-0ab535657690a98519
 """
 
-dl = Downloader(cookie=cookie)
+subtitle_lang = "English"
+
+dl = Downloader(cookie=cookie, subtitle_lang=subtitle_lang)
 
 text = input("Course link here : ")
 
